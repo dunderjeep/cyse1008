f5edb7e278d6d7454093f326c40c974f3c175f2e
dunderjeep <dunderjeep@gmail.com>
2024-01-30 09:45:57 -0500

    jfjfjfjfj

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -3,7 +3,7 @@ import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
 } from "../store"
-import { useDispatch } from "react-redux";  /// fooo barakfdkdkdk
+import { useDispatch } from "react-redux";
 
 function SignInPage() {
   const dispatch = useDispatch();
